82778ee492b0913be0629bf287676aa66873964f
Behzod Nizomiddinov <javaguru1716@gmail.com>
2023-08-19 12:42:27 +0500

    all done

--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -1,3 +1,5 @@
+package streams;
+
 //todo - in this class create two methods that can handle file copying
 // 1.first method must include src file and from it your method will have to
 // create another duplicate file. Then name of duplicate file should be like
@@ -8,6 +10,7 @@
 // 3.For the class involve logger to log method operation details.
 // When your methods throws an exception log error message to the console
 // When copy file successfully finishes log info about it.
+
 import java.io.*;
 import java.nio.file.Files;
 import java.util.logging.ConsoleHandler;
@@ -68,8 +71,8 @@ public class Streams {
 
 
     public static void main(String[] args) throws IOException {
-        File file = new File("src/hello.txt");
+        File file = new File("src/streams/hello.txt");
         duplicateFile(file);
-        copyFile("src/hello.txt","src/copied.txt");
+        copyFile("src/streams/hello.txt","src/streams/copied.txt");
     }
 }
